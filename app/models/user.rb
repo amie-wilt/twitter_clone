f072941565ab487b35682e7b9168c9cf0149b5b9
Amie Wilt <amie.wilt@gmail.com>
2015-06-20 12:11:19 -0400

    added acts as follower gem, set up methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_many :moos
 
+  acts_as_followable
+  acts_as_follower
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
